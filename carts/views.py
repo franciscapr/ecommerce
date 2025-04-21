fb9a6f68370b96f85736a3729f9bca65fb3ea36a
Francisca <francisca.paredes.pr@gmail.com>
2025-04-21 19:16:43 -0400

    actualizacion del precio

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from store.models import Product
 from . models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def _cart_id(request):
@@ -34,5 +35,25 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2*total)/100
+        grand_total = total + tax
+
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+
+    return render(request, 'store/cart.html', context)
