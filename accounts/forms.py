1f723e9162594461a39fcba238895d9e85a58911
Francisca <francisca.paredes.pr@gmail.com>
2025-04-22 13:57:01 -0400

    templates registros

---
+++ accounts/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ingresar Password',
+        'class': 'form-control'
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirmar Password',
+        'class': 'form-control'
+
+    }))
+
+
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder']='Ingrese Nombre'
+        self.fields['last_name'].widget.attrs['placeholder']='Ingrese Apellidos'
+        self.fields['phone_number'].widget.attrs['placeholder']='Telefono'
+        self.fields['email'].widget.attrs['placeholder']='Correo Electronico'
+
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
