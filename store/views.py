757844ce8766490465f18987acc1f3eacae4ae2f
Francisca <francisca.paredes.pr@gmail.com>
2025-04-21 20:41:55 -0400

    .

--- store/views.py
+++ store/views.py
@@ -15,10 +15,10 @@ def store(request, category_slug=None):
     
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        products = Product.objects.filter(category=categories, is_available=True).order_by('id')
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         product_count = products.count()
 
     context = {
