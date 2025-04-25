48c9ca00733474548192225ab930f5e0f8a3f054
Francisca <francisca.paredes.pr@gmail.com>
2025-04-25 14:44:01 -0400

    redireccionamiento de pagina al checkout

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
-
+import requests
 
 
 def register(request):
@@ -100,7 +100,17 @@ def login(request):
 
             auth.login(request, user)
             messages.success(request, 'Inicio de Sesiòn')
-            return redirect('dashboard')
+
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
+
         else:
             messages.error(request, 'Las credenciales son incorrectas')
             return redirect('login')
