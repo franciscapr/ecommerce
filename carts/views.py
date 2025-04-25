070c57ed3d3308cb91981808212a5cacf08917ef
Francisca <francisca.paredes.pr@gmail.com>
2025-04-25 12:04:39 -0400

    carrito de compras y sesion de usuarios

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product, Variation
 from . models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -135,6 +136,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
 
 
 
+@login_required(login_url='login')
 def checkout(request, total=0, quantity=0, cart_items=None):
 
     tax = 0
