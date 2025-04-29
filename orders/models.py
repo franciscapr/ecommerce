36e38f69e0bd06784376a90dcecd58903c0933bd
Francisca <francisca.paredes.pr@gmail.com>
2025-04-29 10:06:05 -0400

    consulta de orden de compra y revision de productos

--- orders/models.py
+++ orders/models.py
@@ -46,6 +46,14 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def full_address(self):
+        return f'{self.addres_line_1} {self.addres_line_2}'
+
+
     def __str__(self):
         return self.first_name
     
